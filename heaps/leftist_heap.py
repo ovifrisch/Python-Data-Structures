e959e00f640c7e176d66dc3aba1461b7e81253e4
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-20 12:49:49 -0700

    merge of leftist heap

--- heaps/leftist_heap.py
+++ heaps/leftist_heap.py
@@ -1,12 +1,27 @@
 
-
+"""
+Heap that allows for efficient merging
+"""
 
 class LeftistHeap:
-	def __init__(self):
+
+	class Node:
+		def __init__(self, data, npl=-1, left=None, right=None):
+			self.data = data
+			self.npl = npl
+			self.left = left
+			self.right = right
+
+	def __init__(self, has_priority):
 		self.root = None
+		self.size = 0
+		self.has_priority = has_priority
 
 	def null_path_length(self, node):
 		# null_path_length(X), of any node X to be the length of the shortest path from X to a node without two children.
+		if (not node):
+			return -1
+
 		q = [{"node":node, "npl":0}]
 
 		while (q):
@@ -15,4 +30,54 @@ class LeftistHeap:
 				return fq['npl']
 			q.append({'node':fq.right, 'npl':fq['npl'] + 1})
 			q.append({'node':fq.left, 'npl':fq['npl'] + 1})
+
+
+	def insert(self, data):
+		pass
+
+	def get_min(self):
+		if (len(self) == 0):
+			raise Exception("Cannot get min of empty heap")
+		return self.head.data
+
+	def remove_min(self, data):
+		pass
+
+
+	"""
+	recursively merge the heap with the larger root with the right subheap of the heap with the smaller root.
+	"""
+	def merge_recursive(self, small, big):
+		if (not small):
+			return big
+		if (not big):
+			return small
+		if (not small.right):
+			small.right = big
+		elif (small.right.get_min() < big.get_min()):
+			small.right = self.merge_recursive(small.right, big)
+		else:
+			small.right = self.merge_recursive(big, small.right)
+		return small
+
+	def merge(self, other):
+		if (len(self) == 0):
+			return other
+		elif (len(other) == 0):
+			return self
+		elif (self.get_min() < other.get_min()):
+			small = self
+			big = other
+		else:
+			small = other
+			big = self
+		return self.merge_recursive(small, big)
+
+
+	def __len__(self):
+		return self.size
+
+	def __repr__(self):
+		res = ""
+		return res
 
