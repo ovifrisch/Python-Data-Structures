2e31cb7a74b54fd2aac6151cc3df4f60929c8ac4
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-20 16:18:43 -0700

    fib heap interface

---
+++ heaps/fibonacci_heap.py
@@ -0,0 +1,28 @@
+
+
+class FibonacciHeap:
+
+	class Node:
+		def __init(self, data):
+			pass
+
+	def __init__(self):
+		pass
+
+
+	def insert(self, data):
+		pass
+
+	def remove_min(self):
+		pass
+
+	def __len__(self):
+		pass
+
+	def __repr__(self):
+		res = ""
+		return res
+
+
+if __name__ == "__main__":
+	h = FibonacciHeap()
