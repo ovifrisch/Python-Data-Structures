a8f6c6e0eb6b2ab62664b9b954cf07fe7fb1b5d4
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-20 17:13:43 -0700

    binomial queue interface

---
+++ heaps/binomial_queue.py
@@ -0,0 +1,29 @@
+import random
+
+class BinomialQueue:
+
+	class Node:
+		def __init__(self, data, left=None, right=None):
+			pass
+
+	def __init__(self):
+		pass
+
+	def insert(self, data):
+		pass
+
+	def remove_min(self, data):
+		pass
+
+	def merge(self, other):
+		pass
+
+	def __len__(self):
+		pass
+
+	def __repr__(self):
+		pass
+
+
+if __name__ == "__main__":
+	bq = BinomialQueue()
