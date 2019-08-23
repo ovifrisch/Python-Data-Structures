d79e69706b03019abdc3e5e52d2e0dcd43582acf
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-22 20:37:53 -0700

    extendable get and contains

--- hashes/extendible_hash.py
+++ hashes/extendible_hash.py
@@ -1,19 +1,109 @@
 
+# from trie import Trie
+
+
+# class MyTrie(Trie):
+# 	def __init__(self):
+# 		Trie.__init__(self)
+
+# 	class Node(Trie.Node):
+# 		def __init__(self):
+
+
+class Trie:
+
+	class Node:
+		def __init__(self, left=None, right=None, data_ptr=None):
+			self.left = left
+			self.right = right
+			self.data_ptr = data_ptr
+
+		def is_leaf(self):
+			return self.data_ptr is not None
+
+	def __init__(self, M):
+		self.M = M
+		self.root = self.Node(data_ptr=Leaf(self.M))
+
+
+class Leaf:
+	def __init__(self, M=4, depth=0):
+		self.M = 4
+		self.depth = 0
+		self.items = []
+
+	def contains(self, key):
+		return key in [x['key'] for x in self.items]
+
+	def __setitem__(self, key, val):
+		for item in self.items:
+			if (item['key'] == key):
+				item['val'] = val
+
+	def __getitem__(self, key):
+		for item in self.items:
+			if (item['key'] == key):
+				return item['val']
+
+	def __len__(self):
+		return len(self.items)
+
+	def add(self, key, val):
+		self.items.append({'key':key, 'val':val})
+
+
 """
 Data too large to fit in main memory
-Analogous to BTree
+Look up only takes 2 disk accesses
 """
 class ExtendibleHash:
 
-	def __init__(self):
+	def __init__(self, M=4):
 		self.size = 0
-
+		self.M = M # the maximum number of elements for a leaf
+		self.directory = Trie(M)
+		self.depth = 0
 
 	def __setitem__(self, key, val):
+		leaf = self.get_leaf(key)
+
+		if (leaf.contains(key)):
+			leaf[key] = val
+			return
+
+		if (len(leaf) < self.M):
+			leaf.add(key, val)
+			return
+
+		# the leaf is at its capacity
 		pass
 
+	def get_leaf(self, key):
+		hashed = self.hash(key)
+		depth = 0
+
+		# find the trie leaf
+		def helper(root):
+			nonlocal key, depth
+			if (root.is_leaf()):
+				return root.data_ptr
+
+			# go right
+			if ((hashed >> depth) & root.bit):
+				root = root.right
+			else:
+				root = root.left
+			depth += 1
+			return helper(root)
+
+		return helper(self.directory.root)
+
+
 	def __getitem__(self, key):
-		pass
+		leaf = self.get_leaf(key)
+		if (not leaf.contains(key)):
+			raise Exception("Key Error")
+		return leaf[key]
 
 	def __repr__(self):
 		pass
@@ -21,8 +111,17 @@ class ExtendibleHash:
 	def __len__(self):
 		return self.size
 
+
+	"""
+	just return the key for now. assuming the keys
+	are all unique integers
+	"""
+	def hash(self, key):
+		return key
+
 	def contains(self, key):
-		pass
+		return self.get_leaf(key).contains(key)
+
 
 	def pop(self, key):
 		pass
@@ -31,3 +130,16 @@ class ExtendibleHash:
 
 if __name__ == "__main__":
 	h = ExtendibleHash()
+	h[0] = 1
+	print(h[0])
+
+
+
+
+
+
+
+
+
+
+
