0c022ca567edf793e2788df40e61ed28dea81097
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-23 20:54:20 -0700

    finished extensible hash

--- hashes/extendible_hash.py
+++ hashes/extendible_hash.py
@@ -1,5 +1,6 @@
 import math
 import copy
+import random
 
 
 class Leaf:
@@ -7,38 +8,45 @@ class Leaf:
 		self.M = M
 		self.depth = depth
 		self.items = []
-		self.min_agreements = float('inf')
-
-	def bits_required(self):
-		return self.depth + self.min_agreements + 1
-
 	"""
-	starting from depth past LSB, return the number of consecutive bits that
-	are the same in hash1 and hash2
+	get the number of bits required to uniquely split
+	this leaf into 2 buckets
+	(this will always be at least self.depth, so return the number of bits after that)
 	"""
-	def num_agreements(self, hash1, hash2):
-		shift = self.depth
-		count = 0
+	def get_agreeing_bits(self):
+		# assume self.items has 2 or more elements
 
-		def agree(bit1, bit2):
-			return (bit1 and bit2) or (not bit1 and not bit2)
+		def helper(hash1, hash2):
+			shift = self.depth
+			count = 0
 
-		while (1):
-			if (agree((hash1 >> shift) & 1, ((hash2 >> shift) & 1))):
-				count += 1
-				shift += 1
-			else:
-				return count
+			def agree(bit1, bit2):
+				return (bit1 and bit2) or (not bit1 and not bit2)
+
+			while (1):
+				if (agree((hash1 >> shift) & 1, ((hash2 >> shift) & 1))):
+					count += 1
+					shift += 1
+				else:
+					return count
+
+
+		agmts = float('inf')
+		for i in range(1, len(self.items)):
+			agmts = min(agmts, helper(self.items[0]['hash'], self.items[i]['hash']))
+		return agmts
 
 	def contains(self, key):
 		return key in [x['key'] for x in self.items]
 
 	def split(self):
 
-		right_leaf = Leaf(M=self.M, depth=self.depth + self.min_agreements + 1)
-		left_leaf = Leaf(M=self.M, depth=self.depth + self.min_agreements + 1)
+		num_agreeing_bits = self.get_agreeing_bits()
 
-		shift = self.depth + self.min_agreements
+		right_leaf = Leaf(M=self.M, depth=self.depth + num_agreeing_bits + 1)
+		left_leaf = Leaf(M=self.M, depth=self.depth + num_agreeing_bits + 1)
+
+		shift = self.depth + num_agreeing_bits
 
 
 		for item in self.items:
@@ -63,13 +71,14 @@ class Leaf:
 	def __len__(self):
 		return len(self.items)
 
+	def remove(self, key):
+		for i in range(len(self.items)):
+			if (self.items[i]['key'] == key):
+				idx = i
+		self.items.pop(idx)
+
 	def add(self, key, val, hashed):
 		self.items.append({'key':key, 'val':val, 'hash':hashed})
-
-		if (len(self) == 1):
-			return
-		else:
-			self.min_agreements = min(self.min_agreements, self.num_agreements(hashed, self.items[0]['hash']))
 
 
 """
@@ -82,9 +91,17 @@ class ExtendibleHash:
 		self.size = 0
 		self.M = M # the maximum number of elements for a leaf
 		self.directory = [Leaf()]
+		self.rand_big_num = random.getrandbits(128) # for hashing
 
 	def hash(self, key):
-		return key
+		key = str(key)
+		def sum_chars(s):
+			res = 0
+			for c in s:
+				res += ord(c)
+			return res
+
+		return sum_chars(key) * self.rand_big_num
 
 
 	def contains(self, key):
@@ -129,13 +146,13 @@ class ExtendibleHash:
 		return idx
 
 
-	def update_directory(self, right_leaf, left_leaf, hashed_key, bits_required):
+	def update_directory(self, right_leaf, left_leaf, hashed_key):
 		old_directory = copy.deepcopy(self.directory)
 		old_bucket = self.get_bucket(hashed_key)
 
 		depth = int(math.log(len(self.directory), 2))
 
-		increase_by = bits_required - depth
+		increase_by = left_leaf.depth - depth
 		step_size = int(pow(2, max(0, increase_by)))
 
 		self.directory = [None] * len(self.directory) * step_size
@@ -169,13 +186,14 @@ class ExtendibleHash:
 		leaf.add(key, val, hashed_key)
 
 		if (len(leaf) <= self.M):
+			self.size += 1
 			return
 
 		# the leaf is at its capacity
-		bits_required = leaf.bits_required()
 		right_leaf, left_leaf = leaf.split()
 
-		self.update_directory(right_leaf, left_leaf, hashed_key, bits_required)
+		self.update_directory(right_leaf, left_leaf, hashed_key)
+		self.size += 1
 
 
 	def __getitem__(self, key):
@@ -188,20 +206,58 @@ class ExtendibleHash:
 	def __len__(self):
 		return self.size
 
+	def __repr__(self):
+		res = "{"
+		for i in range(len(self.directory)):
+			leaf = self.directory[i]
+			for j in range(len(leaf.items)):
+				item = leaf.items[j]
+				key = item['key']
+				val = item['val']
+				if (isinstance(key, int)):
+					res += str(key)
+				else:
+					res += "'" + str(key) + "'"
+				res += ": "
+
+				if (isinstance(val, int)):
+					res += str(val)
+				else:
+					res += "'" + str(val) + "'"
+				res += ", "
+
+		if (len(res) > 1):
+			res = res[:-2]
+		res += "}"
+		return res
+
+
+	# lazy delete
 	def pop(self, key):
-		pass
+		hashed_key = self.hash(key)
+		if (not self.contains(key)):
+			raise Exception("Key Error: ".format(key))
+
+		self.get_leaf(hashed_key).remove(key)
+		self.size -= 1
 
 
 if __name__ == "__main__":
-	h = ExtendibleHash()
+	h = ExtendibleHash(M=4)
 	h[0] = 1
 	h[1] = 1
 	h[3] = 1
 	h[2] = 1
 	h[11] = 1
+	h.pop(11)
+	h.pop(0)
+	h[0] = 1
+	print(len(h))
 
 	for i in range(len(h.directory)):
-		print(h.directory[i].items, h.directory[i].depth, h.directory[i].min_agreements)
+		print(h.directory[i].items, h.directory[i].depth)
+
+	print(h)
 
 
 
