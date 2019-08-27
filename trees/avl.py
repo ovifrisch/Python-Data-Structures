e6c3abb5929e4996805f89d56f93b50e83f08d61
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-27 15:32:17 -0700

    doc for avl

--- trees/avl.py
+++ trees/avl.py
@@ -1,3 +1,27 @@
+"""
+
+AVL Tree
+________
+Self-balancing Binaray Search Tree
+__________________________________
+Insertion: O(logN)
+Deletion: O(logN)
+_________________
+What it's good for:
+
+Guaranteeing O(logN)
+time complexity on Binary Search Tree
+operations
+_______________________________________
+How it works:
+
+For each node in the tree, we enforce that
+the height of its two children differ by at
+most 1. This AVL property can be violated
+on insertion and deltion, so we fix it using
+rotations along the search path.
+
+"""
 
 
 class AVL:
@@ -9,7 +33,6 @@ class AVL:
 			self.left = left
 			self.right = right
 
-	# height of empty tree is defined to be -1
 	def __init__(self, data=[]):
 		self.size = 0
 		self.root = None
@@ -17,46 +40,85 @@ class AVL:
 		for x in data:
 			self.insert(x)
 
+	def insert(self, val):
 
-	def is_balanced(self):
-
-		def helper(root):
+		def helper(root, val):
+			# found spot to insert
 			if (not root):
-				return True
-			if (abs(self.height(root.left) - self.height(root.right)) > 1):
+				root = self.Node(val, 0)
+				self.size += 1
+
+			# val already in the tree; raise exception
+			elif (root.val == val):
+				raise Exception("Value already exists")
+
+			# go left
+			elif (val < root.val):
+				root.left = helper(root.left, val)
+
+			# go right
+			else:
+				root.right = helper(root.right, val)
+
+			# balance the current node if there is a height violation
+			# called for every node on the insertion path
+			return self.balance(root)
+
+		# the helper method returns the new root
+		self.root = helper(self.root, val)
+
+	def remove(self, val):
+
+		# aux method to find the minimum of a subtree
+		def find_min(node):
+			while (node.left): # keep going left
+				node = node.left
+			return node.val # can't go left anymore
+
+		def helper(root, target):
+			nonlocal find_min
+			if (not root):
+				raise Exception("Value not found")
+
+			# go left
+			elif (target < root.val):
+				root.left = helper(root.left, target)
+
+			# go right
+			elif (target > root.val):
+				root.right = helper(root.right, target)
+
+			# target is equal to root.val
+			else:
+				# two children; replace with min of right,
+				# recursively delete the replacement
+				if (root.left and root.right):
+					root.val = find_min(root.right)
+					root.right = helper(root.right, root.val)
+				else:
+					if (root.left):
+						return root.left
+					else:
+						return root.right
+			return self.balance(root)
+
+		# helper will return the new root
+		self.root = helper(self.root, val)
+		self.size -= 1
+
+	def contains(self, val):
+
+		def helper(root, target):
+			if (not root):
 				return False
+			if (target == root.val):
+				return True
+			if (target < root.val):
+				return helper(root.left, target)
+			else:
+				return helper(root.right, target)
 
-			return helper(root.left) and helper(root.right)
-		return helper(self.root)
-
-	def rotateWithLeft(self, root):
-		new_root = root.left
-		root.left = new_root.right
-		new_root.right = root
-		new_root.right.height = 1 + max(self.height(new_root.right.right), self.height(new_root.right.left))
-		root = new_root
-		return root
-
-	def rotateWithRight(self, root):
-		new_root = root.right
-		root.right = new_root.left
-		new_root.left = root
-		new_root.left.height = 1 + max(self.height(new_root.left.left), self.height(new_root.left.right))
-		root = new_root
-		return root
-
-	def doubleRotateWithLeft(self, root):
-		root.left = self.rotateWithRight(root.left)
-		return self.rotateWithLeft(root)
-
-	def doubleRotateWithRight(self, root):
-		root.right = self.rotateWithLeft(root.right)
-		return self.rotateWithRight(root)
-
-	def height(self, node):
-		if (not node):
-			return -1
-		return node.height
+		return helper(self.root, val)
 
 	def balance(self, root):
 		if (not root):
@@ -87,31 +149,34 @@ class AVL:
 		root.height = 1 + max(self.height(root.left), self.height(root.right))
 		return root
 
+	def rotateWithLeft(self, root):
+		new_root = root.left
+		root.left = new_root.right
+		new_root.right = root
+		new_root.right.height = 1 + max(self.height(new_root.right.right), self.height(new_root.right.left))
+		root = new_root
+		return root
 
-	def insert(self, val):
+	def rotateWithRight(self, root):
+		new_root = root.right
+		root.right = new_root.left
+		new_root.left = root
+		new_root.left.height = 1 + max(self.height(new_root.left.left), self.height(new_root.left.right))
+		root = new_root
+		return root
 
-		def helper(root, val):
-			if (not root):
-				root = self.Node(val, 0)
-				self.size += 1
-			elif (root.val == val):
-				raise Exception("Value already exists")
-			elif (val < root.val):
-				root.left = helper(root.left, val)
-			else:
-				root.right = helper(root.right, val)
+	def doubleRotateWithLeft(self, root):
+		root.left = self.rotateWithRight(root.left)
+		return self.rotateWithLeft(root)
 
-			return self.balance(root)
+	def doubleRotateWithRight(self, root):
+		root.right = self.rotateWithLeft(root.right)
+		return self.rotateWithRight(root)
 
-		self.root = helper(self.root, val)
-
-	def find_min(self, node):
-		while (node.left):
-			node = node.left
-		return node.val
-
-
-
+	def height(self, node):
+		if (not node):
+			return -1
+		return node.height
 
 	"""
 	return the inorder traversal of the
@@ -127,58 +192,6 @@ class AVL:
 			helper(root.right)
 		helper(self.root)
 		return res
-
-	def __next__(self):
-		def helper(root):
-			if (not root):
-				return
-			helper(root.left)
-			yield root.val
-			helper(root.right)
-		helper(self.root)
-		yield self
-
-	def __iter__(self):
-		return self
-
-
-	def remove(self, val):
-
-		def helper(root, target):
-			if (not root):
-				raise Exception("Value not found")
-
-			elif (target < root.val):
-				root.left = helper(root.left, target)
-			elif (target > root.val):
-				root.right = helper(root.right, target)
-			else:
-				if (root.left and root.right):
-					root.val = self.find_min(root.right)
-					root.right = helper(root.right, root.val)
-				else:
-					if (root.left):
-						return root.left
-					else:
-						return root.right
-			return self.balance(root)
-
-		self.root = helper(self.root, val)
-		self.size -= 1
-
-	def contains(self, val):
-
-		def helper(root, target):
-			if (not root):
-				return False
-			if (target == root.val):
-				return True
-			if (target < root.val):
-				return helper(root.left, target)
-			else:
-				return helper(root.right, target)
-
-		return helper(self.root, val)
 
 
 	"""
@@ -215,8 +228,8 @@ if __name__ == "__main__":
 	for i in range(10):
 		avl.insert(i)
 
-	for node in avl:
-		print(node)
+	for i in range(10):
+		avl.remove(i)
 
 
 
