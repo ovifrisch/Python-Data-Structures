b660fb2ce7a566e09727b4fb991387dae485d0c3
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-09-14 16:51:02 -0700

    done range tree add and query

--- trees/range_tree/range_tree.py
+++ trees/range_tree/range_tree.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class TreeNode:
 
-    def __init__(self, val, dim, left=None, right=None, inner=None):
+    def __init__(self, val, dim=0, left=None, right=None, inner=None):
         self.val = val
         self.dim = dim
         self.left = left
@@ -27,9 +27,8 @@ class RangeTree:
         if (not isinstance(num_dimensions, int) and num_dimensions <= 1):
             raise Exception("num_dimensions must be an Int")
         self.dims = num_dimensions
-        if data is None:
-            self.root = None
-        else:
+        self.root = None
+        if data is not None:
             if not isinstance(data, np.ndarray):
                 raise Exception("data must be a numpy array")
             if (len(data.shape) == 1 and self.dims > 1):
@@ -41,75 +40,58 @@ class RangeTree:
             np.apply_along_axis(self.add, axis=1, arr=data)
 
     def add(self, data):
-        pass
+        """
+        [2, 4, 9]
+        [5, 54, 17]
+        """
+        def add_helper(data, dim, tree):
+            if dim == self.dims:
+                return None
+
+            # empty tree, insert here and recursively insert on empty inner tree
+            if tree is None:
+                tree = TreeNode(data, dim)
+                tree.inner = add_helper(data, dim + 1, None)
+                return tree
+
+
+            def binary_search(node):
+                if not node:
+                    tree_node = TreeNode(data, dim)
+                    tree_node.inner = add_helper(data, dim + 1, None)
+                    return tree_node
+
+                # insert into the inner node because we are adding to the outer's subtree
+                node.inner = add_helper(data, dim + 1, node.inner)
+
+                if (not node.left and not node.right):
+                    if data[dim] <= node.val[dim]:
+                        node.left = TreeNode(data, dim)
+                        node.right = TreeNode(node.val, dim)
+                        node.left.inner = add_helper(data, dim + 1, None)
+                        node.right.inner = add_helper(node.val, dim + 1, None)
+                        node.val = data
+                    else:
+                        node.left = TreeNode(node.val, dim)
+                        node.right = TreeNode(data, dim)
+                        node.left.inner = add_helper(node.val, dim + 1, None)
+                        node.right.inner = add_helper(data, dim + 1, None)
+
+                else:
+                    if (data[dim] < node.val[dim]):
+                        node.left = binary_search(node.left)
+                    else:
+                        node.right = binary_search(node.right)
+
+                return node
+
+            tree = binary_search(tree)
+            return tree
+
+        self.root = add_helper(data, 0, self.root)
 
     def remove(self, data):
         pass
-
-
-
-    def get_points1d(self, minimum, maximum):
-
-        subtrees = []
-        result = []
-
-        def min_traverse(node):
-            if not node:
-                return
-
-            if minimum > node.val:
-                min_traverse(node.right)
-            else:
-                subtrees.append(node.right)
-                min_traverse(node.left)
-
-        def max_traverse(node):
-            if not node:
-                return
-
-            if (maximum < node.val):
-                max_traverse(node.left)
-            else:
-                subtrees.append(node.left)
-                max_traverse(node.right)
-
-        def traverse(node):
-            if not node:
-                return
-
-            if (minimum < node.val and maximum > node.val):
-                min_traverse(node.left)
-                max_traverse(node.right)
-            elif (minimum < node.val and maximum < node.val):
-                traverse(node.left)
-            elif (minimum > node.val and maximum > node.val):
-                traverse(node.right)
-            elif (minimum == node.val and maximum == node.val):
-                result.append(node.val)
-                traverse(node.right)
-            elif (minimum == node.val):
-                result.append(node.val)
-                max_traverse(node.right)
-            elif (maximum == node.val):
-                min_traverse(node.left)
-
-
-        def bfs(node):
-            if not node:
-                return
-
-            bfs(node.left)
-            result.append(node.val)
-            bfs(node.right)
-
-
-        traverse(self.root)
-        for subtree in subtrees:
-            dfs(subtree)
-
-        return sorted(result)
-
-
 
 
 
@@ -128,25 +110,123 @@ class RangeTree:
 
         # validation code
 
-        result = []
+        subtrees = []
 
-        def helper(node):
+        def max_traverse(node):
+            maximum = range_[1][node.dim]
+            dim_val = node.val[node.dim]
+            bottom = node.dim == self.dims - 1
 
             if not node:
                 return
 
-            left_bound = range_[0][node.dim]
-            right_bound = range_[1][node.dim]
-            if (left_bound > node.val):
-                helper(node.right)
-            elif (right_bound < node.val):
-                helper(node.left)
+            if (not node.left and not node.right):
+                if dim_val <= maximum:
+                    if bottom:
+                        subtrees.append(node)
+                    else:
+                        traverse(node.inner)
+                return
+
+            if dim_val <= maximum:
+                if bottom:
+                    subtrees.append(node.left)
+                else:
+                    if node.left:
+                        traverse(node.left.inner)
+                max_traverse(node.right)
+
+            else:
+                max_traverse(node.left)
+
+        def min_traverse(node):
+            minimum = range_[0][node.dim]
+            dim_val = node.val[node.dim]
+            bottom = node.dim == self.dims - 1
+
+            if not node:
+                return
+
+            if (not node.left and not node.right):
+                if (dim_val >= minimum):
+                    if (bottom):
+                        subtrees.append(node)
+                    else:
+                        traverse(node.inner)
+                return
+
+            if dim_val >= minimum:
+                if bottom:
+                    subtrees.append(node.right)
+                else:
+                    if node.right:
+                        traverse(node.right.inner)
+                min_traverse(node.left)
+            else:
+                min_traverse(node.right)
+
+        def traverse(node):
+            if not node:
+                return
+
+            minimum = range_[0][node.dim]
+            maximum = range_[1][node.dim]
+            dim_val = node.val[node.dim]
 
 
+            if (not node.left and not node.right):
+                if (dim_val >= minimum and dim_val <= maximum):
+                    if (node.dim == self.dims - 1):
+                        subtrees.append(node)
+                    else:
+                        traverse(node.inner)
+                return
+
+            if (minimum < dim_val and maximum < dim_val):
+                traverse(node.left)
+            elif (minimum > dim_val and maximum > dim_val):
+                traverse(node.right)
+            elif (minimum < dim_val and maximum > dim_val):
+                min_traverse(node.left)
+                max_traverse(node.right)
+            elif (minimum == dim_val and maximum == dim_val):
+                traverse(node.right)
+                traverse(node.left)
+            elif (minimum == dim_val):
+                max_traverse(node.right)
+                traverse(node.left)
+            elif (maximum == dim_val):
+                min_traverse(node.left)
+                traverse(node.right)
+
+        traverse(self.root)
+
+        """go over all subtrees and add the leaves"""
+        result = np.zeros((1, self.dims))
+
+        def dfs(node):
+            nonlocal result
+            if not node:
+                return
+
+            if (not node.left and not node.right): #leaf
+                result = np.vstack([result, node.val.reshape((1, self.dims))])
+            dfs(node.left)
+            dfs(node.right)
+
+        for tree in subtrees:
+            dfs(tree)
+
+        return result[1:]
 
 
 if __name__ == "__main__":
-    t = RangeTree(num_dimensions=3, data=np.array([[1, 2, 3], [4, 5, 6]]))
+    data = np.array([[5, 54, 17], [2, 4, 9]])
+    range_ = (np.array([1, 2, 4]), np.array([10, 53, 100]))
+    t = RangeTree(3, data)
+    res = t.get_points(range_)
+    print(res)
+
 
 
 
---
+++ trees/range_tree/tests/test.py
@@ -0,0 +1,71 @@
+import unittest
+import numpy as np
+import sys
+sys.path.append("./../")
+from range_tree import RangeTree
+import random
+
+class TestRangeTree(unittest.TestCase):
+
+    """
+    """
+
+    def __init(self, *args, **kwargs):
+        super(TestIntervalTree, self).__init__(*args, **kwargs)
+        self.tree = RangeTree()
+
+
+    def contained_in(self, intervals, range_):
+        result = np.ones((1, range_[0].shape[0]))
+        for interval in intervals:
+            good = True
+            for i in range(interval.shape[0]):
+                if (not(interval[i] >= range_[0][i] and interval[i] <= range_[1][i])):
+                    good = False
+                    break
+            if good:
+                result = np.vstack([result, interval])
+        return result[1:]
+
+
+    def random_test_gen(self, num_intervals, num_dims, min_range, max_range):
+        """return a list of num_intervals random intervals and a random range
+           such that each interval and the range is between the allowed range.
+        """
+
+        intervals = np.zeros((num_intervals, num_dims))
+        minimum = np.random.randint(min_range, high=max_range + 1, size=num_dims)
+        maximum = np.zeros((num_dims,))
+        for i in range(num_dims):
+            maximum[i] = random.randint(minimum[i], max_range)
+
+        for i in range(num_intervals):
+            interval = np.zeros((1, num_dims))
+            for j in range(num_dims):
+                interval[0, j] = random.randint(min_range, max_range)
+            intervals[i, :] = interval
+
+        return intervals, (minimum, maximum)
+
+    def test1(self):
+        data = np.array([[5, 54, 17], [2, 4, 9]])
+        range_ = (np.array([1, 2, 4]), np.array([10, 53, 100]))
+        t = RangeTree(3, data)
+        res = t.get_points(range_)
+        self.assertTrue(np.array_equal(res, self.contained_in(data, range_)))
+
+
+    def test2(self):
+        dims = 3
+        data, range_ = self.random_test_gen(1000, dims, 1, 10)
+        # range_ = (np.array([1, 1, 1]), np.array([8, 8, 8]))
+        t = RangeTree(dims, data)
+        res = t.get_points(range_)
+        print(res.shape)
+        res = set([tuple(row) for row in res])
+        truth = set([tuple(row) for row in self.contained_in(data, range_)])
+        self.assertTrue(np.array_equal(res, truth))
+
+
+if __name__ == "__main__":
+    unittest.main()
